bfe387a7c93b49b158614a1c14a8106919e39d5f
Mohamed Elsayed <m.elsaied@gmail.com>
2020-04-23 00:58:45 -0500

    Revert "Backend"

--- src/main/java/com/example/SpringReact/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SpringReact.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.SpringReact.model.Category;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
