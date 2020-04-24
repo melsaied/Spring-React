b1111001e1183161aadd61fbafd28b4ba0e6923f
First Last <email.mail.com>
2020-04-24 18:27:26 -0500

    updating categories list component

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import Category from "./controller/Category";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Category from './controller/Category';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Category />
+    <Category/>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
